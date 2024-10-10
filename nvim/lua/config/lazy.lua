2da637e28039d5561dc7ef683e67ad3b9cc77719
Bavajithu <bavajithuarchlinux@gmail.com>
2024-10-10 07:48:28 +0530

    Add files via upload

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,7 +17,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "solarized-osaka",
+    } },
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
   },
